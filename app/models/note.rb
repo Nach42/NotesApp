a810a49135034597dcb40c860b79eeccaa7d09c4
Pablo <pablo.marcos1@alu.uclm.es>
2018-04-19 10:42:24 +0200

    añadido borrado en cascada para las notas

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :user
-  has_many :has_collections
+  has_many :has_collections,dependent: :delete_all
   has_many :collections, through: :has_collections
   validates :title, presence: true 
   validates :body, presence: true
