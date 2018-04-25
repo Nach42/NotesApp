6a4be0cdbfac92a99dd22a6df2d241b1e7009d9b
Ignacio Dones <ignacio.dones@alu.uclm.es>
2018-04-25 17:08:10 +0200

    añadir imagen a note sin paperclip

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,6 +5,9 @@ class Note < ApplicationRecord
   validates :title, presence: true 
   validates :body, presence: true
 
+  #has_attached_file :photo, styles: {ancho: "500x300", largo: "300x500"}
+  #validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+
   FOTOS = File.join Rails.root, 'public', 'photo_store'
   after_save :guardar_foto
 
