c85f03c9ccc7b1c5bb8d4e1a0d38b39795759f00
pacoml94 <fco.murillo@alu.uclm.es>
2018-04-25 19:21:59 +0200

    Registro cambiado a inglés

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411142712) do
+ActiveRecord::Schema.define(version: 20180425145821) do
 
   create_table "collections", force: :cascade do |t|
     t.string "name"
