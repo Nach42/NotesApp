88a60c6471fe418b8376ef271d0dc1926d417047
Pablo <pablo.marcos1@alu.uclm.es>
2018-05-04 09:54:42 +0200

    añadida gema y lógica de modelo para añadir amigos. Falta implementar las partes de controller y vistas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426154201) do
+ActiveRecord::Schema.define(version: 20180504065846) do
 
   create_table "collections", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 20180426154201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_collections_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.string "friendable_type"
+    t.integer "friendable_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "blocker_id"
+    t.integer "status"
+    t.index ["friendable_type", "friendable_id"], name: "index_friendships_on_friendable_type_and_friendable_id"
   end
 
   create_table "has_collections", force: :cascade do |t|
@@ -40,11 +51,6 @@ ActiveRecord::Schema.define(version: 20180426154201) do
     t.integer "photo_file_size"
     t.datetime "photo_updated_at"
     t.index ["user_id"], name: "index_notes_on_user_id"
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
   end
 
   create_table "users", force: :cascade do |t|
