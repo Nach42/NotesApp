0bc4cacc3ca4c02206e1230fc7364eb5e485b187
Pablo <pablo.marcos1@alu.uclm.es>
2018-05-15 12:40:27 +0200

    primeras mejoras de apariencia

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20180504065846) do
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "extension"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
